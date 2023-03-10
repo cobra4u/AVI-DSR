81387a3a4f7bcb93539c4506fe145f677449636c
Divjot Singh <94235610+cobra4u@users.noreply.github.com>
2023-03-10 09:57:52 +0530

    Update updown.js

--- 9-mar/Increment dec/updown.js
+++ 9-mar/Increment dec/updown.js
@@ -1,5 +1,20 @@
-function myfunction1() {
-    const prompt = require("prompt-sync")(); 
-    var f = prompt();
-    
+var value = 0;
+function myFunction1() {
+        value = value + 1;
+        document.getElementById("computerScore").innerHTML = value;
+
+}
+function myFunction2() {
+        value = value - 1;
+        document.getElementById("computerScore").innerHTML = value;
+        if (value < 0) {
+            value = 0;
+            document.getElementById("computerScore").innerHTML = value;
+        }
+
+}
+function myFunction3() {
+        value = 0;
+        document.getElementById("computerScore").innerHTML = value;
+
 }
