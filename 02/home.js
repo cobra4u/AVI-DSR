b0012685e478ab32b96304d3f2e7601b303dba63
Divjot Singh <94235610+cobra4u@users.noreply.github.com>
2023-03-14 18:06:13 +0530

    Delete 02 directory

--- 02/home.js
+++
@@ -1,9 +0,0 @@
-let welcome = 'welcome';
-
-let price = 49.9;
-    fly = 'boots',
-    discounting = false;
-
-
-showmessage(price);
-console.log(price);
--- 02/utils.js
+++
@@ -1,4 +0,0 @@
-function showmessage(message) {
-    document.getElementById('message').textcontent = message;
-}
-
