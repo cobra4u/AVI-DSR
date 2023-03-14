d82d65e73d5902ba1310ea6ef0da579c31625958
Divjot Singh <94235610+cobra4u@users.noreply.github.com>
2023-03-14 18:03:53 +0530

    Add files via upload

---
+++ 06/home.js
@@ -0,0 +1,47 @@
+
+// let i = -4;
+// do {
+//     console.log(i);
+//     i--;
+// } while (i > 0);
+// let state = 'FL';
+// let taxPercent = 0;
+
+// if (1.1 +1.3 !== 2.4) {
+//     showMessage('true');
+
+// }
+// let price = 55;
+// let message = price > 10 ? 'yes' : 'No';
+// showMessage(message)
+
+// let message = 1;
+// showMessage(message);
+
+// if(true){
+//     let value = 'Yes';
+//     showMessage(value);
+
+// }
+// console.log(value);
+
+//FOR//
+
+// for (let i=0; i<3;i++) {
+//     console.log(i)
+// }
+
+// for (let i=6; i5; i++) {
+//     console.log(i); 
+// }
+
+
+
+
+
+if ( 55!=4){
+    console.log("tt")
+}
+else {
+    console.log("hdgds")
+}
---
+++ 08/home.js
@@ -0,0 +1,118 @@
+
+// const button = document.getElementById('see-review');
+
+// button.addEventListener('click', function() {
+
+//     const review = document.getElementById('review');
+
+//     if (review.classList.contains('d-none')) {
+//         review.classList.remove('d-none');
+//         button.textContent = 'CLOSE REVIEW';
+//     }
+//     else {
+//         review.classList.add('d-none');
+//         button.textContent = 'SEE REVIEW';
+//     }
+    
+
+// });
+
+// let person = {
+//     name :"John",
+//     age : 32,
+//     parttime : false,
+//     [symbol] : 'secretinfo'
+
+// };
+
+// person['age'] = 44;
+// showMessage(person.age);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.parttime);
+
+// let person = {
+//     name :"John",
+//     age : 32,
+//     parttime : false,
+//     showInfo: function() {
+//         showMessage(this.name + realAge);
+//     }
+// };
+
+// person.showInfo(34);
+
+// showMessage(typeof person.showInfo);
+
+// let message = 'Hello';
+
+// function changesmesage(message){
+//     message = 'Hi';
+// }
+
+// // changesmesage(message);
+
+
+// showMessage(message);
+
+
+
+
+
+// let person = {
+//     name :"John",
+//     age : 30,
+//     parttime : false,
+// }
+
+// function incrementAge(person){
+//     person.age++;
+// }
+
+// incrementAge(person);
+// showMessage(person.age);
+
+
+
+
+// let now=new Date()
+// showMessage(now.toDateString());
+
+
+
+// showMessage( Math.random (  ));
+
+// let s ='Hello';
+// showMessage(s.charAt(1));
+
+// const header = document.getElementById('message');
+
+// header.style.fontWeight='700';
+
+
+
+
+// const button = document.getElementById('See-review');
+
+// button.addEventListener('click', function(){
+
+//     const review = document.getElementById('review');
+//     if (review.classList.contains('d-none')){
+//         review.classList.remove('d-none');
+//         button.textContent ='CLOSE REVIEW';
+
+//     }
+//     else{
+//         review.classList.add('d-none');
+//         button.textContent ='SEE REVIEW';
+
+//     }
+    
+
+// });
+
+
+
+
+
+
