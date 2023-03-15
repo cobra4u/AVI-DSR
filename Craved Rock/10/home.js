73d87b62aae89deedf2be1df112e018ea407003a
Divjot Singh <94235610+cobra4u@users.noreply.github.com>
2023-03-15 16:44:33 +0530

    Add files via upload

---
+++ Craved Rock/10/home.js
@@ -0,0 +1,77 @@
+// 'use strict';
+
+// const productId = 1234;
+
+// console.log(productId);
+
+
+// global scope
+// let productId = 12345;
+
+// function showProductId(){
+//       console.log(productId);
+
+// }
+
+// showProductId
+
+
+
+
+
+// const app = {
+//     productId: 12345,
+//     userName: 'Joe',
+//     orderNumber: 789
+// };
+
+// function showProductId() {
+//     console.log(app.productId);
+// }
+
+// showProductId(); 
+
+
+
+
+// function showProductId(){
+//     let productId=12345;
+
+
+//     function fix(){ 
+//         let productId = 45678;
+//         console.log('in fix:', productId);
+
+    
+//     }
+
+
+//     console.log(productId);
+
+// }
+
+// showProductId(1234);
+
+
+
+// var hoisting
+
+
+
+
+// productId = 456;
+
+// console.log(productId);
+
+// let productId = 123;
+
+
+showProductId();
+
+function showProductId() {
+    console.log(123);
+}
+
+
+
+
