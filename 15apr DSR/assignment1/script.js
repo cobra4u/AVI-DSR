824f9d356cfdad09932304a16c4568beb923385a
Divjot Singh <94235610+cobra4u@users.noreply.github.com>
2023-04-15 19:28:36 +0530

    Add files via upload

---
+++ 15apr DSR/assignment1/script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function hide(){
+    var y = document.getElementById("myDIV");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+      } else {
+        y.style.display = "none";
+      }
+    }
